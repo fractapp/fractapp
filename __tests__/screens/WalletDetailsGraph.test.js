3c03bb7502f68570dc1837e64aa57ef5e72e90c4
yura6904 <45623333+yura6904@users.noreply.github.com>
2021-05-30 19:55:52 +0300

    Test for new WalletDetailsGraph

---
+++ __tests__/screens/WalletDetailsGraph.test.js
@@ -0,0 +1,363 @@
+import React, {useContext, useState} from 'react';
+import renderer from 'react-test-renderer';
+import {WalletDetailsGraph} from 'screens/WalletDetailsGraph';
+import {Currency, Wallet} from 'types/wallet';
+import GlobalStore, {initialState} from 'storage/Global';
+import ChatsStore from 'storage/Chats';
+import {TxStatus, TxType} from 'types/transaction';
+import {fireEvent, render} from '@testing-library/react-native';
+
+jest.mock('storage/DB', () => ({}));
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useState: jest.fn(),
+  useContext: jest.fn(),
+  Linking: jest.fn(() => ({
+    openURL: jest.fn(),
+  })),
+}));
+jest.mock('utils/backend', () => ({
+  getImgUrl: jest.fn(() => 'userAvatarMock'),
+}));
+
+useState.mockImplementation((init) => [init, jest.fn()]);
+
+it('Test view with empty state', () => {
+  useContext.mockReturnValueOnce({
+    state: GlobalStore.initialState(),
+  });
+  useContext.mockReturnValueOnce({
+    state: ChatsStore.initialState(),
+  });
+  
+  const wallets = [
+    new Wallet(
+      'Wallet Polkadot',
+      'address#1',
+      Currency.DOT,
+      100,
+      '1000000000000',
+      10,
+      0
+    ),
+    new Wallet(
+      'Wallet KSM',
+      'address#2',
+      Currency.KSM,
+      200,
+      '3000000000000',
+      20,
+      0
+    )
+  ];
+
+  const tree = renderer
+    .create(
+      <WalletDetailsGraph
+        navigation={null}
+        route={{
+          params: {
+            wallets: wallets
+          },
+        }}
+      />,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+  
+});
+
+it('Test view with 1 wallet', () => {
+  useContext.mockReturnValueOnce({
+    state: GlobalStore.initialState(),
+  });
+  useContext.mockReturnValueOnce({
+    state: ChatsStore.initialState(),
+  });
+  
+  const wallets = [
+    new Wallet(
+      'Wallet Polkadot',
+      'address#1',
+      Currency.DOT,
+      100,
+      '1000000000000',
+      10,
+      0
+    )
+  ];
+
+  const txs = new Map();
+  txs.set(
+    '1',
+    {
+      id: '1',
+      userId: null,
+      address: 'address#1',
+      currency: Currency.DOT,
+      txType: TxType.Sent,
+      timestamp: new Date('02-12-2020').getTime(),
+      value: 12,
+      usdValue: 12,
+      fee: 12,
+      usdFee: 12,
+      status: TxStatus.Fail,
+    }
+  );
+
+  const chatsState = ChatsStore.initialState();
+  chatsState.transactions.set(
+    txs.keys(),
+    {
+      transactionById: txs.values()
+    }
+  );
+  useContext.mockReturnValueOnce({
+    state: {
+      transactions: chatsState.transactions,
+      isInitialized: true,
+    },
+    dispatch: () => null,
+  });
+
+  const tree = renderer
+    .create(
+      <WalletDetailsGraph
+        navigation={null}
+        route={{
+          params: {
+            wallets: wallets
+          },
+        }}
+      />,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+  
+});
+
+it('Test view with txs', () => {
+  const wallets = [
+    new Wallet(
+      'Wallet Polkadot',
+      'address#1',
+      Currency.DOT,
+      100,
+      '1000000000000',
+      10,
+      0
+    ),
+    new Wallet(
+      'Wallet KSM',
+      'address#2',
+      Currency.KSM,
+      200,
+      '3000000000000',
+      20,
+      0
+    )
+  ];
+
+  const globalState = GlobalStore.initialState();
+  globalState.users.set('userId', {
+    id: 'id',
+    name: 'name',
+    username: 'username',
+    avatarExt: 'png',
+    lastUpdate: new Date('12-12-2020').getTime(),
+    addresses: {
+      0: 'address#2DOT',
+      1: 'address#2KSM',
+    },
+  });
+  
+  const txs = new Map();
+  txs.set(
+    '1',
+    {
+      id: '1',
+      userId: null,
+      address: 'address#1',
+      currency: Currency.DOT,
+      txType: TxType.Sent,
+      timestamp: new Date('02-12-2020').getTime(),
+      value: 12,
+      usdValue: 12,
+      fee: 12,
+      usdFee: 12,
+      status: TxStatus.Fail,
+    }
+  );
+  txs.set(
+    '2',
+      {
+        id: '2',
+        userId: 'userId',
+        address: 'address#2DOT',
+        currency: Currency.DOT,
+        txType: TxType.Sent,
+        timestamp: new Date('03-12-2020').getTime(),
+        value: 10,
+        usdValue: 10,
+        fee: 10,
+        usdFee: 10,
+        status: TxStatus.Success,
+      }
+  );
+
+  const chatsState = ChatsStore.initialState();
+  for (let [key, value] of txs.entries()) {
+    chatsState.transactions.set(
+      key,
+      {
+        transactionById: value
+      }
+    );
+  }
+
+  useContext.mockReturnValueOnce({
+    state: globalState,
+    dispatch: () => null,
+  });
+  useContext.mockReturnValueOnce({
+    state: {
+      transactions: chatsState.transactions,
+      isInitialized: true,
+    },
+    dispatch: () => null,
+  });
+
+  const tree = renderer
+    .create(
+      <WalletDetailsGraph
+        navigation={null}
+        route={{
+          params: {
+            wallets: wallets
+          },
+        }}
+      />,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('Test view click txs', () => {
+  const wallets = [
+    new Wallet(
+      'Wallet Polkadot',
+      'address#1',
+      Currency.DOT,
+      100,
+      '1000000000000',
+      10,
+      0
+    ),
+    new Wallet(
+      'Wallet KSM',
+      'address#2',
+      Currency.KSM,
+      200,
+      '3000000000000',
+      20,
+      0
+    )
+  ];
+  const txs = new Map();
+  txs.set(
+    '1',
+    {
+      id: '1',
+      userId: null,
+      address: 'address#1',
+      currency: Currency.DOT,
+      txType: TxType.Sent,
+      timestamp: new Date('02-12-2020').getTime(),
+      value: 12,
+      usdValue: 12,
+      fee: 12,
+      usdFee: 12,
+      status: TxStatus.Fail,
+    }
+  );
+  txs.set(
+    '2',
+      {
+        id: '2',
+        userId: 'userId',
+        address: 'address#2DOT',
+        currency: Currency.DOT,
+        txType: TxType.Sent,
+        timestamp: new Date('03-12-2020').getTime(),
+        value: 10,
+        usdValue: 10,
+        fee: 10,
+        usdFee: 10,
+        status: TxStatus.Success,
+      }
+  );
+
+  const chatsState = ChatsStore.initialState();
+  for (let [key, value] of txs.entries()) {
+    chatsState.transactions.set(
+      key,
+      {
+        transactionById: value
+      }
+    );
+  }
+
+  const globalState = GlobalStore.initialState();
+  globalState.users.set('userId', {
+    id: 'id',
+    name: 'name',
+    username: 'username',
+    avatarExt: 'png',
+    lastUpdate: new Date('12-12-2020').getTime(),
+    addresses: {
+      0: 'address#2DOT',
+      1: 'address#2KSM',
+    },
+  });
+
+  useContext.mockReturnValueOnce({
+    state: globalState,
+    dispatch: () => null,
+  });
+  useContext.mockReturnValueOnce({
+    state: {
+      transactions: chatsState.transactions,
+      isInitialized: true,
+    },
+    dispatch: () => null,
+  });
+  
+  const navigate = jest.fn();
+  const component = render(
+    <WalletDetailsGraph
+      navigation={{
+        navigate: navigate,
+      }}
+      route={{
+        params: {
+          wallets: wallets,
+        },
+      }}
+    />,
+  );
+
+  fireEvent.press(component.getByText('address#1'));
+  expect(navigate).toBeCalledWith('TransactionDetails', {
+    transaction: txs.get('1'),
+    wallets: wallets[this.transaction.currency],
+    user: null,
+  });
+
+  fireEvent.press(component.getByText('name'));
+  expect(navigate).toBeCalledWith('TransactionDetails', {
+    transaction: txs.get('2'),
+    wallets: wallets[transaction.currency],
+    user: globalState.users.get('userId'),
+  });
+});
+
